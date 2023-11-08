0622763f0718a40b0eb2b31d93372fbc482994c3
montahamet <montaha.metjaouel@esprit.tn>
2023-11-08 19:32:36 +0100

    index annonce inv

--- src/Form/AnnonceinvestissementType.php
+++ src/Form/AnnonceinvestissementType.php
@@ -6,6 +6,7 @@ use App\Entity\Annonceinvestissement;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
 
 class AnnonceinvestissementType extends AbstractType
 {
@@ -17,7 +18,11 @@ class AnnonceinvestissementType extends AbstractType
             ->add('datepublication')
             ->add('localisation')
             ->add('description')
-            ->add('photo')
+            ->add('photo', FileType::class, [
+                'label' => 'Upload an image',
+                'mapped' => false, // To prevent the form from trying to set this as a property on your entity
+                'required' => false, // Allow the field to be optional
+            ])
             ->add('iduser')
         ;
     }
