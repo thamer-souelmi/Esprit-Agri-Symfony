8d62219fd3a806ed62ccfcc4fb108cd1690a8203
montahamet <montaha.metjaouel@esprit.tn>
2023-11-14 00:15:08 +0100

    Annoce inv + ajout negociationnnnnnn

---
+++ src/Form/UserType.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\User;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class UserType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('cin')
+            ->add('nom')
+            ->add('prenom')
+            ->add('mdp')
+            ->add('mail')
+            ->add('adresse')
+            ->add('numtel')
+            ->add('role', ChoiceType::class, [
+                'choices' => [
+                    
+                    'Agriculteur' => 'agriculteur',
+                    'User' => 'user',
+                ],
+            ])
+            ->add('image', FileType::class, [
+                'label' => 'User Image (JPG, JPEG, PNG file)',
+                'mapped' => false,
+                'required' => false,
+                'attr' => ['accept' => 'image/*'],
+            ])
+            ->add('save',SubmitType::class)
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => User::class,
+        ]);
+    }
+}
