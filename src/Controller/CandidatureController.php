4a37ee0cfbf3f0905ab6b372a4b75c7447fd99b8
khmili <khmili.hamza@esprit.tn>
2023-11-24 08:28:29 -1100

    allah iraba7

--- src/Controller/CandidatureController.php
+++ src/Controller/CandidatureController.php
@@ -2,11 +2,15 @@
 
 namespace App\Controller;
 
+use App\Entity\Annoncerecrutement;
 use App\Entity\Candidature;
+use App\Entity\Ouvrier;
 use App\Form\CandidatureType;
 use App\Repository\CandidatureRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\File\UploadedFile;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -21,15 +25,53 @@ class CandidatureController extends AbstractController
             'candidatures' => $candidatureRepository->findAll(),
         ]);
     }
-
-    #[Route('/new', name: 'app_candidature_new', methods: ['GET', 'POST'])]
-    public function new(Request $request, EntityManagerInterface $entityManager): Response
+    #[Route('/back', name: 'app_candidatureback_index', methods: ['GET'])]
+    public function indexb(CandidatureRepository $candidatureRepository): Response
     {
+        return $this->render('candidature/indexback.html.twig', [
+            'candidatures' => $candidatureRepository->findAll(),
+        ]);
+    }
+    #[Route('/new/{idrecurt}', name: 'app_candidature_new', methods: ['GET', 'POST'])]
+    public function new(Request $request, EntityManagerInterface $entityManager , $idrecurt): Response
+    {        //$idrecurt = $request->get('idRecurt');
         $candidature = new Candidature();
+
+        $ouvrier = $entityManager->getRepository(ouvrier::class)->find(1);
+        $candidature ->setIdouvrierfor($ouvrier);
+
+        $annoncerecrutement = $entityManager->getRepository(Annoncerecrutement::class)->find($idrecurt);
+        $candidature ->setIdannrecru($annoncerecrutement);
         $form = $this->createForm(CandidatureType::class, $candidature);
         $form->handleRequest($request);
+        $currentDate = new \DateTime();
+        $candidature->setDatecandidature($currentDate);
+        if ($form->isSubmitted() && $form->isValid()) {
+            // Handle file upload
+            $file = $form->get('certifforma')->getData();
+            
+            if ($file instanceof UploadedFile) {
+                $fileName = uniqid().'.'.$file->guessExtension();
+
+                // Move the file to the desired directory
+                $file->move(
+                    'img/',
+                    $fileName
+                );
+
+                // Save the image file name to the entity
+                $candidature->setCertifforma($fileName);
+            }
+
+            $entityManager->persist($candidature);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('app_annoncerecrutement_index', [], Response::HTTP_SEE_OTHER);
+        }
 
         if ($form->isSubmitted() && $form->isValid()) {
+            //$candidature -> setIdannrecru($annoncerecrutement);
+
             $entityManager->persist($candidature);
             $entityManager->flush();
 
@@ -67,17 +109,7 @@ class CandidatureController extends AbstractController
             'form' => $form,
         ]);
     }
-   #[Route('/all', name: 'app_candidature_get_all', methods: ['GET'])]
-public function getAll(CandidatureRepository $repo): Response
-{
-    // Récupérez toutes les candidatures depuis la base de données
-    $list = $repo->findAll();
 
-    // Rendez la vue appropriée avec la liste des candidatures
-    return $this->render('candidature/getAll.html.twig', [
-        'candidatures' => $list,
-    ]);
-}
 
     #[Route('/{idcandidature}', name: 'app_candidature_delete', methods: ['POST'])]
     public function delete(Request $request, Candidature $candidature, EntityManagerInterface $entityManager): Response
@@ -89,4 +121,36 @@ public function getAll(CandidatureRepository $repo): Response
 
         return $this->redirectToRoute('app_candidature_index', [], Response::HTTP_SEE_OTHER);
     }
+
+   /**
+ * @Route("/candidature/{id}/{decision}", name="app_candidature_decision", methods={"GET"})
+ */
+public function handleCandidatureDecision(int $id, string $decision, EntityManagerInterface $entityManager): RedirectResponse
+{
+    // Check if the user is an admin (you can implement your own logic to check admin access)
+
+    $candidature = $entityManager->getRepository(Candidature::class)->find($id);
+    //var_dump($candidature);
+    //die;
+    if (!$candidature) {
+        throw $this->createNotFoundException('Candidature not found');
+    }
+
+    // Update the candidacy's "accepte" field based on the admin's decision
+    if ($decision === 'accept') {
+        $candidature->setStatusCandidature(true);
+        
+    } elseif ($decision === 'refuse') {
+        $candidature->setStatusCandidature(false);
+    }
+
+    $entityManager->flush();
+
+    $this->addFlash('success', 'Candidature decision has been saved.');
+
+    // Redirect the admin back to the page that displays the candidates for the specific concours
+    // You might want to replace 'your_route_name' with the actual route name you want to redirect to
+    return $this->redirectToRoute('app_candidature_index');
+}
+
 }
